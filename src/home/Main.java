e25918790ccefa8546f2aa4ac2f37a6c390caaaf
Ishikawa Tatsuki <papaymango@gmail.com>
2021-11-15 16:52:30 +0900

    connect mysql server

--- src/home/Main.java
+++ src/home/Main.java
@@ -9,8 +9,12 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.nio.file.Paths;
+import java.sql.*;
 
 public class Main extends Application {
+
+    private static String uri = "jdbc:mysql://192.168.1.3:3306/tungsten_db";
+    private static String driver = "com.mysql.cj.jdbc.Driver";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -31,6 +35,33 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        System.out.println("main");
+        try{
+            Class.forName(driver);
+            Connection connection = DriverManager.getConnection(uri,"user001","rmml0321");
+//            Connection connection = DriverManager.getConnection(uri);
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from tungsten");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                System.out.println(resultSet);
+            }
+            for (int i = 6; i <= 9;i++){
+                String sql = "insert into tungsten(lot,update_date,producr_date,quantity,quality,method) values('MWx1600" + i +"',now(),now(),14,'A','2F電気分解'"+")";
+//                String sql = "insert into tungsten(lot,quantity,quality,method) values ('MWx16002',14,'A','2F電気分解');";
+                System.out.println(sql);
+                preparedStatement = connection.prepareStatement(sql);
+                System.out.println("a");
+                System.out.println(preparedStatement.executeUpdate());
+                while (resultSet.next()){
+                    System.out.println(resultSet);
+                }
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         launch(args);
     }
 }
